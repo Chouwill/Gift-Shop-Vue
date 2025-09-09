b7854e09ba9cfc3f3d0e3c6f51f243a2f2933bbf
Chouwill <will92068@gmail.com>
2025-09-09 16:15:21 +0800

    chore(deploy): 調整 Render 部署環境與 .env 設定

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
 });
--- src/store/Products.ts
+++ src/store/Products.ts
@@ -1,6 +1,20 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
+import { getProducts } from "../api/method";
 
-export const useProductStore = defineStore('alerts', {
+export const useProductStore = defineStore("alerts", () => {
+  async function fetchProducts() {
+    try {
+      const res = await getProducts();
 
-})
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return {
+    fetchProducts,
+  };
+});
