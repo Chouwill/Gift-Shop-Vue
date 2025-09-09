bed763ee1fc5fff5a2b39ce23ecc340629e8a606
Chouwill <will92068@gmail.com>
2025-09-09 17:10:00 +0800

    fix(router): 修復 router-view class 傳遞警告

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -3,12 +3,13 @@ import HeroBanner from "../components/IndexPage/HeroBanner.vue";
 import CategoryHighlight from "../components/IndexPage/CategoryHighlight.vue";
 
 import CategoryGrid from "../components/IndexPage/CategoryGrid.vue";
-
 </script>
 
 <template>
-  <HeroBanner />
-  <CategoryHighlight />
-  <CategoryGrid />
-  <!-- <h2>首頁</h2> -->
+  <div>
+    <HeroBanner />
+    <CategoryHighlight />
+    <CategoryGrid />
+    <!-- <h2>首頁</h2> -->
+  </div>
 </template>
