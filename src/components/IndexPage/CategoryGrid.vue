d82360f6898816eff3dfe5a1ebf65558cb41fd49
Chouwill <will92068@gmail.com>
2025-09-09 14:14:58 +0800

    wip：CategoryGrid 元件初始版，尚未串 API

---
+++ src/components/IndexPage/CategoryGrid.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="category-grid-box border w-full mt-15 flex justify-center items-center">
+    <div class="grid-box w-[956px] flex flex-wrap">
+        <div class="w-1/2 bg-green-500 h-[478px]"></div>
+        <div class="w-1/2 bg-red-500 h-[478px]"></div>
+        <div class="w-1/2 bg-red-500 h-[478px]"></div>
+        <div class="w-1/2 bg-green-500 h-[478px]"></div>
+
+    </div>
+  </div>
+</template>
--- src/page/Index.vue
+++ src/page/Index.vue
@@ -2,10 +2,13 @@
 import HeroBanner from "../components/IndexPage/HeroBanner.vue";
 import CategoryHighlight from "../components/IndexPage/CategoryHighlight.vue";
 
+import CategoryGrid from "../components/IndexPage/CategoryGrid.vue";
+
 </script>
 
 <template>
   <HeroBanner />
   <CategoryHighlight />
-  <h2>首頁</h2>
+  <CategoryGrid />
+  <!-- <h2>首頁</h2> -->
 </template>
