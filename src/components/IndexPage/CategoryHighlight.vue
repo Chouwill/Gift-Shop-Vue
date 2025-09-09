5ffd47bc24336b5c57438c1144191265569978ec
Chouwill <will92068@gmail.com>
2025-09-09 13:40:57 +0800

    feat:新增商品分類區塊UI組件

---
+++ src/components/IndexPage/CategoryHighlight.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="category-title w-full h-[600px]">
+    <h2 class="text-center text-4xl my-5">100美元以內的禮物</h2>
+
+    <div class="title-image w-full h-[550px]">
+      <img
+        class="w-full h-full object-cover"
+        src="../../assets/img/category-bg.png"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
--- src/page/Index.vue
+++ src/page/Index.vue
@@ -1,8 +1,11 @@
 <script setup>
 import HeroBanner from "../components/IndexPage/HeroBanner.vue";
+import CategoryHighlight from "../components/IndexPage/CategoryHighlight.vue";
+
 </script>
 
 <template>
   <HeroBanner />
+  <CategoryHighlight />
   <h2>首頁</h2>
 </template>
