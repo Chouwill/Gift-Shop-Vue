c2d1e939e399f245be030f19e862943ce5f38a5e
Chouwill <will92068@gmail.com>
2025-09-23 18:53:25 +0800

    feat:新增首頁商品輪播

--- src/components/IndexPage/LookCarousel.vue
+++ src/components/IndexPage/LookCarousel.vue
@@ -5,32 +5,55 @@ const propsItems = defineProps(["card"]);
 
 const cardItems = ref(propsItems.card);
 
+const page = ref(1);
+
+const itemsGroup = 7;
+
+function autoPlayCarousel() {
+  //   setInterval(() => {
+  //     page.value++;
+  //   }, 2000);
+}
+
+function nextPage() {
+  //   autoPlayCarousel();
+  setInterval(() => {
+    page.value++;
+  }, 2000);
+}
+function returnPage() {
+  page.value--;
+}
+
 onMounted(() => {
   console.log("props2", cardItems.value);
 });
 
 const computedItems = computed(() => {
-  return cardItems.value.slice(0, 7);
+  return cardItems.value.slice(page.value - 1, page.value * itemsGroup);
+  //   return cardItems.value.slice(page - 1, page * itemsGroup);
 });
+
+nextPage();
 </script>
 
 <template>
   <div class="carousel-box w-full">
-    <div class="text-center flex justify-center items-center gap-5 w-full  my-4">
+    <div class="text-center flex justify-center items-center gap-5 w-full my-4">
       <h2 class="text-4xl">Look</h2>
       <h2 class="text-4xl">PPC</h2>
+      <button @click="nextPage">下一頁</button>
+      <button @click="returnPage">上一頁</button>
     </div>
 
-    <div
-      class="carousel flex gap-4 w-full overflow-x-hidden md:overflow-x-visible"
-    >
+    <div class="carousel flex gap-4 w-full overflow-x-hidden">
       <div
         class="md:w-[210px] md:h-[210px] w-full flex justify-center items-center flex-shrink-0"
         v-for="item in computedItems"
         :key="item.id"
       >
         <img
-          class="md:w-[210px] md:h-[210px] w-[250px] h-[250px] object-cover first:ps-4 last:pe-4"
+          class="md:w-[250px] md:h-[250px] w-[250px] h-[250px] object-cover first:ps-4 last:pe-4"
           :src="item.image_url"
           alt=""
         />
