560bdb732147fd80730ba34d81b5f8ee87cb6b88
Chouwill <will92068@gmail.com>
2025-09-27 16:26:14 +0800

    feat:新增使用Flowbite UI套件並測試套用

--- src/components/ShoppingMall/ShopFilter.vue
+++ src/components/ShoppingMall/ShopFilter.vue
@@ -1,4 +1,28 @@
-<script setup></script>
+<script setup>
+import { FwbAlert } from "flowbite-vue";
+import { ref } from "vue";
+import { FwbSelect } from "flowbite-vue";
 
+const selected = ref("");
+const countries = [
+  { value: "us", name: "United States" },
+  { value: "ca", name: "Canada" },
+  { value: "fr", name: "France" },
+];
+</script>
 
-<template>篩選組件</template>
+<template>
+  <div class="filter-container">
+    <fwb-alert type="success">
+      Success! You can now use Flowbite Vue in your Vue application 🎉
+    </fwb-alert>
+
+    <fwb-select
+      v-model="selected"
+      :options="countries"
+      label="Select a country"
+      size="md"
+      class="w-[200px] bg-red-900"
+    />
+  </div>
+</template>
