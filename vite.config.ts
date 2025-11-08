04ed446ca753dc27e1b9349885223054610bc909
Chouwill <will92068@gmail.com>
2025-11-08 23:31:19 +0800

    fix: connect frontend to render backend

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://gift-shop-backend-api.onrender.com",
         changeOrigin: true
       }
     }
