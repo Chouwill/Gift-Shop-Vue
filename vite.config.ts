39331de1cc2f07fdac0eb3993d122b95d8db7e7e
Chouwill <will92068@gmail.com>
2025-11-08 23:18:34 +0800

    uddate1108

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:3000",
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true
       }
     }
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 10000,
+    allowedHosts: ["*"]
   }
 });
