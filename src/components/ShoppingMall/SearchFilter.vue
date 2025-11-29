349a3766fe4597c1d8fbaaf9e324b546b5517778
Chouwill <will92068@gmail.com>
2025-11-29 11:29:40 +0800

    feat: add SearchFilter componet

---
+++ src/components/ShoppingMall/SearchFilter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full bg-gray-100 px-4 py-3 rounded-xl shadow-lg translate-y-[2px] transition duration-200">
+    <div class="search-filter-box flex justify-center items-center ">
+      <fieldset class="fieldset w-[400px] ">
+        <legend class="fieldset-legend text-sm">商品分類</legend>
+        <select class="select !border">
+          <option disabled selected>全部分類</option>
+          <option>Chrome</option>
+          <option>FireFox</option>
+          <option>Safari</option>
+        </select>
+        <!-- <span class="label">Optional</span> -->
+      </fieldset>
+      <fieldset class="fieldset w-[400px]">
+        <legend class="fieldset-legend text-sm">價格篩選</legend>
+        <select class="select !border">
+          <option disabled selected>全部分類</option>
+          <option>Chrome</option>
+          <option>FireFox</option>
+          <option>Safari</option>
+        </select>
+        <!-- <span class="label">Optional</span> -->
+      </fieldset>
+    </div>
+  </div>
+</template>
--- src/page/ShoppingMall.vue
+++ src/page/ShoppingMall.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchFilter from "@/components/ShoppingMall/SearchFilter.vue";
 
+</script>
 
 <template>
-  <div>
-    商城
+  <div class="md:w-[1200px] border mx-auto my-5">
+    <SearchFilter />
   </div>
 </template>
