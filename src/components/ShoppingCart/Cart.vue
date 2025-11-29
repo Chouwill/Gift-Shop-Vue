6c1271abd58aa14ce6e5be9c86f7f785d2fb1523
Chouwill <will92068@gmail.com>
2025-11-29 23:55:55 +0800

    text RWD

--- src/components/ShoppingCart/Cart.vue
+++ src/components/ShoppingCart/Cart.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
 import { useProductStore } from "@/stores/useCart";
 
+const modal = ref(true);
+const dialogRef = ref<HTMLDialogElement | null>(null);
 const productStore = useProductStore();
 productStore.fetchProduct();
+
+function openCart() {}
+
+// 組件掛載時自動開啟 dialog
+onMounted(() => {
+  if (dialogRef.value && modal.value) {
+    dialogRef.value.showModal();
+  }
+});
 </script>
 
 <template>
   <div>
-    <!-- Open the modal using ID.showModal() method -->
-    <button class="btn" onclick="my_modal_5.showModal()">open modal</button>
+    <button class="btn" @click="openCart">open modal</button>
     <dialog
+      ref="dialogRef"
       id="my_modal_5"
       class="modal modal-bottom sm:modal-middle border relative"
+      v-if="modal"
     >
       <div class="modal-box w-11/12 max-w-2xl">
         <div
@@ -20,7 +33,7 @@ productStore.fetchProduct();
           <h2 class="modal-title text-left font-medium ml-4">購物車</h2>
           <button class="mr-4"><i class="fa-solid fa-x"></i></button>
         </div>
-        <div class=" mt-7">
+        <div class="mt-7">
           <table class="table">
             <!-- head -->
             <thead>
@@ -33,9 +46,15 @@ productStore.fetchProduct();
             </thead>
             <tbody>
               <!-- row 1 -->
-              <tr v-for="(item, idx) in productStore.products" :key="item.id" class="hover:bg-amber-400 w-[90%]">
+              <tr
+                v-for="(item, idx) in productStore.products"
+                :key="item.id"
+                class="hover:bg-amber-400 w-[90%]"
+              >
                 <td class="" rowspan="1">
-                  <div class="md:w-[254px] w-[100px] flex justify-start items-center gap-5 !border">
+                  <div
+                    class="md:w-[254px] w-[100px] flex justify-start items-center gap-5 !border"
+                  >
                     <div class="img-box">
                       <img
                         :src="item.image_url"
